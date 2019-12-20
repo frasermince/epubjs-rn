14d09869eb594b45aee023b602dab311a15b1e6a
Fraser Mince <fraser@thoughtbot.com>
2019-12-20 12:49:05 -0600

    Add overflow hidden.

--- src/Rendition.js
+++ src/Rendition.js
@@ -30,6 +30,7 @@ const embeddedHtml = (script, bridge) => `
   <script>${bridge}</script>
   <style>
     body {
+      overflow-x: hidden;
       margin: 0;
       -webkit-tap-highlight-color: rgba(0,0,0,0);
       -webkit-tap-highlight-color: transparent; /* For some Androids */
